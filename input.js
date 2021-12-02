fd6f7debe46ac62f08a3f2839bc512c88b5385be
houseofsam <samxsaid@gmail.com>
2021-12-02 05:26:11 +0000

    changed serupInput() to accept connect() object that  lets user interact with server. Passed connect() return value into setupInput in play.js. Also switched handleUserInput to to switch case syntax and added keys to display messaged in snake game

--- input.js
+++ input.js
@@ -1,11 +1,34 @@
+let connection;
+
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
+  switch (key) {
+    case '\u0003': process.exit();
+    case 'w':
+      connection.write('Move: up');
+      break;
+    case 'a':
+      connection.write('Move: left');
+      break;
+    case 's':
+      connection.write('Move: down');
+      break;
+    case 'd':
+      connection.write('Move: right');
+      break;
+    case 'h':
+      connection.write('Say: Hello!');
+      break;
+    case 's':
+      connection.write('Beat my score!');
+    case 'b':
+      connection.write('I\'m the best!');
+    
   }
-}
+};
 
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
